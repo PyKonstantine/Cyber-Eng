95dd4ed8c027c21c03a52b5552c1327201d5ce5e
Cyber-Ego <chai.ne.voron1991@gmail.com>
2021-02-01 00:56:03 +0300

    Описанно два объекта: Контроль света, и бокс ввода текста.

--- main2.py
+++ main2.py
@@ -1,8 +1,5 @@
 from settings import *
-from objects import *
-
-
-lite_control = LiteControlButton(20, 300, 20, 225)
+from objects import LiteControlButton, InputBox
 
 
 def main():
@@ -13,16 +10,21 @@ def main():
     clock = pygame.time.Clock()
     done = False
 
+    lite_control = LiteControlButton(20, 300, 20, 225)
+    input_text = InputBox(10, 200, WIDTH - 10, 23)
+
     while not done:
         for event in pygame.event.get():
             lite_control.handle_event(event)
+            input_text.handle_event(event)
             if event.type == pygame.QUIT:
                 done = True
 
         sc.fill((lite_control.get_lite_value()))
         lite_control.draw(sc)
-        pygame.display.flip()
-        clock.tick(30)
+        input_text.draw(sc)
+        pygame.display.update()
+        clock.tick(FPS)
 
 
 if __name__ == '__main__':
--- objects.py
+++ objects.py
@@ -1,45 +1,80 @@
-import pygame as pg
-from settings import HEIGHT
+from settings import *
 
-COLOR_INACTIVE = pg.Color('lightskyblue3')
-COLOR_ACTIVE = pg.Color('dodgerblue2')
+pygame.init()
+font = pygame.font.Font(FONT, FONT_SIZE)
 
 
 class LiteControlButton:
+    """
+    Есть проблема с перемещением объекта по оси У. проблема связана с выходом из цветового диапазона - нужно
+    обратобать универсальность значения цвета с полажением ползунка.
+    """
 
     def __init__(self, x, y, w, h):
-        self.rect = pg.Rect(x, y, w, h)
+        self.rect = pygame.Rect(x, y, w, h)
         self.R = 0
         self.G = 255
         self.B = 0
         self.button_y = 20
         self.x = x
         self.y = y
-        self.color = COLOR_INACTIVE
+        self.color = DARK_GRAY
         self.active = False
-        self.aria = pg.Surface((w, h))
+        self.aria = pygame.Surface((w, h))
 
     def handle_event(self, event):
-        if event.type == pg.MOUSEBUTTONDOWN:
+        if event.type == pygame.MOUSEBUTTONDOWN:
             if self.rect.collidepoint(event.pos):
                 self.active = not self.active
-                self.color = COLOR_ACTIVE
+                self.color = SKY_BLUE
                 self.button_y = event.pos[-1] + (self.y - HEIGHT)
                 self.R = self.button_y
                 self.G = 225 - self.R
-                print(self.button_y)
-                print(self.G, self.R)
+                print(self.button_y)  # tmp
+                print(self.G, self.R)  # tmp
             else:
                 self.active = False
-                self.color = COLOR_INACTIVE
-                print('inactive')
+                self.color = DARK_GRAY
+                print('inactive')  # tmp
 
     def draw(self, screen):
         self.aria.fill((self.get_lite_value()))
-        pg.draw.rect(screen, self.color, self.rect, 5)
-        pg.draw.circle(self.aria, (self.R, self.G, self.B), (self.rect.w // 2, self.button_y), (self.rect.w // 2), 20)
+        pygame.draw.rect(screen, self.color, self.rect, 5)
+        pygame.draw.circle(self.aria, (self.R, self.G, self.B), (self.rect.w // 2, self.button_y), (self.rect.w // 2), 20)
         screen.blit(self.aria, (self.x, self.y))
 
     def get_lite_value(self):
         return self.G, self.G, self.G
 
+
+class InputBox:
+
+    def __init__(self, x, y, w, h, text=''):
+        self.rect = pygame.Rect(x, y, w, h)
+        self.color = DARK_GRAY
+        self.text = text
+        self.txt_surface = font.render(text, True, self.color)
+        self.active = False
+
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.rect.collidepoint(event.pos):
+                self.active = not self.active
+            else:
+                self.active = False
+            self.color = SKY_BLUE if self.active else DARK_GRAY
+        if event.type == pygame.KEYDOWN:
+            if self.active:
+                if event.key == pygame.K_RETURN:
+                    print(self.text)
+                    self.text = ''
+                elif event.key == pygame.K_BACKSPACE:
+                    self.text = self.text[:-1]
+                else:
+                    self.text += event.unicode
+                self.txt_surface = font.render(self.text, True, self.color)
+
+    def draw(self, screen):
+        screen.blit(self.txt_surface, (self.rect.x+5, self.rect.y+5))
+        pygame.draw.line(screen, self.color, (self.rect.x, self.rect.y + self.rect.h),
+                         (self.rect.w, self.rect.y + self.rect.h), 2)
